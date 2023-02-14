fd26ef4142ceae24b67843e50cf27e391e2d4b44
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-02-14 21:51:20 +0100

    Connect to tribaloo

---
+++ Public/Connect-TribelooUser.ps1
@@ -0,0 +1,13 @@
+﻿function Connect-TribelooUser {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Token,
+        [Parameter(Mandatory)][alias('Url', 'BaseUri')][string] $Uri
+    )
+
+    $Script:AuthorizationTribeloo = @{
+        'Authorization' = "Bearer $Token"
+        'Content-Type'  = 'application/json; charset=utf-8'
+        'BaseUri'       = $Uri
+    }
+}
